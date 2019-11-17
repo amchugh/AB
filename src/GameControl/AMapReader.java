1bd4bfd2460892f725f4b12ec4e023f1e11129ec
Aidan <mchughaw@gmail.com>
2019-11-17 12:57:25 -0800

    Changed the project structure so it would work for me :)

--- src/GameControl/AMapReader.java
+++ src/GameControl/AMapReader.java
@@ -5,8 +5,6 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Iterator;
