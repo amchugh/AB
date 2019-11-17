27df4c7631c8b82b6c28201e9e84d6bbcbac31ed
Jason McHugh <mchughj@gmail.com>
2019-11-16 21:34:58 -0800

    Adding some simple documentation.

--- src/GameControl/AMap.java
+++ src/GameControl/AMap.java
@@ -2,6 +2,13 @@ package GameControl;
 
 import java.awt.*;
 
+// This is the interface definition for a Map instance.  A Map represents an object which is updated every step
+// and which can be drawn onto the display.  This is a very generic definition.
+//
+// TODO:  This is too generic and really represents a "I am a generic thing that can be displayed".  It is likely
+// TODO:  that this AMap will be augmented in the future to make it more specific.  In case it doesn't we should
+// TODO:  replace with a more generic interface name.
+
 public interface AMap {
   void update();
   void draw(Graphics g);
